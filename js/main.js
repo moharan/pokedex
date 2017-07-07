a8f5195881ff485324ed511151bdcc427055a3a7
RangiBecerra <rangibecerra@gmail.com>
2017-07-07 12:47:52 -0400

    imagen y nombre

--- js/main.js
+++ js/main.js
@@ -1,29 +1,17 @@
 //alert("existe");
+// 
 $(document).ready(function() {
 	/**/
-	$.get("http://pokeapi.co/api/v2/pokedex/1/", function(x) {
-		console.log(x);
+	$.get("https://pokeapi.co/api/v2/pokedex/1/", function(x) {
+		//console.log(x);
 		// array 721
-		x.pokemon_entries.forEach(function(element){
+		x.pokemon_entries.forEach(function(element, i){
 			// elemento
 			console.log(element.pokemon_species.name[0]);
-			$(".pokemon").append("<div class='elemento'>" + element.pokemon_species.name + "</div>")
+			var numero = i + 1;
+			$(".pokemon").append("<div class='elemento'><img src='http://pokeapi.co/media/img/" + numero + ".png'>" + element.pokemon_species.name + "</div>")
+			// http://pokeapi.co/media/img/3.png working img
+			//$(".pokemon").append("<img src='http://pokeapi.co/media/img/" + numero + ".png'>")
 		})
 	});
-	/*ajax
-	$.ajax({
-		url: '/path/to/file',
-		type: 'default GET (Other values: POST)',
-		dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
-		data: {param1: 'value1'},
-	})
-	.done(function() {
-		console.log("success");
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
-	});*/
 });
