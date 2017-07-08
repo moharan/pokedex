12d3a9b3a6ad46a917c6b1e68d86cfa1ebf702d7
RangiBecerra <rangibecerra@gmail.com>
2017-07-08 01:13:05 -0400

    texto descripcion en todos los idiomas

--- js/main.js
+++ js/main.js
@@ -3,16 +3,14 @@
 $(document).ready(function() {
 	/**/
 	$.get("https://pokeapi.co/api/v2/pokedex/1/", function(x) {
-		//console.log(x);
+		console.log(x);
 		// array 721
 		x.pokemon_entries.forEach(function(element, i){
 			// elemento
 			//console.log(element.pokemon_species.name[0]);
 			var numero = i + 1;
 			$(".pokemon").append("<div class='elemento text-center'><a href='#' id='modal" + numero + "'><img class='img-responsive' src='http://pokeapi.co/media/img/" + numero + ".png'>" + element.pokemon_species.name + "</a></div>")
-			//$(this).addClass("parrafo-box");
-			// http://pokeapi.co/media/img/3.png working img
-			//$(".pokemon").append("<img src='http://pokeapi.co/media/img/" + numero + ".png'>")
+			//modal
 			$("#modal" + numero).click(function() {
 				console.log("existe" + numero);
 				$("#myModal").modal("show");
@@ -20,10 +18,15 @@ $(document).ready(function() {
 				$(".modal-title").append("<h2 class='titulo'>" + element.pokemon_species.name + "</h2>");
 				$( "img" ).remove( "#modal-img" );
 				$(".modal-body").append("<img class='img-responsive' id='modal-img' src='http://pokeapi.co/media/img/" + numero + ".png'>");
-				
+				// información modal
 				$.get("https://pokeapi.co/api/v2/pokemon-species/1/", function(y) {
 					// segundo recorrido de ajax
 						console.log(y);
+						y.flavor_text_entries.forEach(function(element, o){
+							var numero = i + 1;
+							//element.flavor_text (todos los textos en distintos idiomas)
+							$(".modal-body").append("<p>" + element.flavor_text + "</p>")
+						});
 				});
 			});
 		})
